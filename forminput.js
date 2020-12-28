498c4dcb3aaf8ac516d9b63fe7584dae9906d7e2
Pascalt22 <72594953+Pascalt22@users.noreply.github.com>
2020-12-28 20:00:18 +0700

    Add files via upload

---
+++ forminput.js
@@ -0,0 +1,9 @@
+function terimainput() {
+    var nama = document.getElementById("nama").value;
+    var mail = document.getElementById("mail").value;
+    var findus = document.getElementById("findus").value;
+    var message = document.getElementById("message").value;
+
+    alert( "Name : " + nama + "\nEmail : " + mail + "\nDestination : " + findus + "\nNote : " + message + "\nPlease check your email");
+    
+}
